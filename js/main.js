c00a754b6cc20115bf13cd63299597350a76ce56
Jay Park <jayhpark99@gmail.com>
2021-11-09 03:32:47 +0000

    users can view a list of champions

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var $parentRow = document.querySelector('.row');
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion.json');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function renderChampions() {
+  var championData = xhr.response;
+  for (var key in championData.data) {
+    var $columnHalf = document.createElement('div');
+    $columnHalf.className = 'column-half';
+
+    var $card = document.createElement('div');
+    $card.className = 'card';
+    $columnHalf.appendChild($card);
+
+    var $name = document.createElement('h2');
+    $name.textContent = key;
+    var $img = document.createElement('img');
+    $img.setAttribute('src', 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + key + '_0.jpg');
+    $card.appendChild($name);
+    $card.appendChild($img);
+    $parentRow.appendChild($columnHalf);
+  }
+});
+xhr.send();
