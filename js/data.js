c597f61f30f165f3c51d23c2333df9234e628515
Jay Park <jayhpark99@gmail.com>
2021-11-10 05:03:14 +0000

    WORK IN PROGRESS

--- js/data.js
+++ js/data.js
@@ -1,1 +1,16 @@
 /* exported data */
+var data = {
+  allChampionData: null
+};
+
+window.addEventListener('beforeunload', handleUnload);
+function handleUnload(event) {
+  var myStorage = window.localStorage;
+  var JSONdata = JSON.stringify(data);
+  myStorage.setItem('javascript-local-storage', JSONdata);
+}
+
+var previousData = localStorage.getItem('javascript-local-storage');
+if (previousData !== null) {
+  data = JSON.parse(previousData);
+}
