a37d654267f394b55868b066206dd6023b3b4ea3
Jay Park <jayhpark99@gmail.com>
2021-11-12 04:22:47 +0000

    ... idk what im doing

--- js/main.js
+++ js/main.js
@@ -26,8 +26,13 @@ function renderChampions() {
     $title.textContent = capitalizeFirstLetter(data.allChampionData.data[key].title);
     var $lore = document.createElement('p');
     $lore.textContent = data.allChampionData.data[key].blurb;
+    $lore.className = 'off';
     var $img = document.createElement('img');
     $img.setAttribute('src', 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/' + key + '_0.jpg');
+    var $flipIcon = document.createElement('i');
+    $flipIcon.className = 'fas fa-angle-right';
+
+    $card.appendChild($flipIcon);
     $card.appendChild($lore);
     $card.appendChild($title);
     $card.appendChild($name);
@@ -48,8 +53,13 @@ function handleCardClick(event) {
         $allButtons[i].className = 'role selected';
       } else { $allButtons[i].className = 'role'; }
     }
-  } else if (event.target.tagName === 'IMG' || event.target.tagName === 'P') {
+  } else if (event.target.closest('div').className.includes('card') &&
+  !event.target.closest('div').className.includes('flipped') &&
+  event.target.tagName !== 'I'
+  ) {
     toggleLore();
+  } else if (event.target.className === 'fas fa-angle-right') {
+    flipCard();
   }
 }
 
@@ -77,7 +87,7 @@ function handleInput(event) {
 function showSearchedChampions(name) {
   var currentTag = document.querySelector('.selected');
   for (var i = 0; i < $allCards.length; i++) {
-    if ($allCards[i].childNodes[0].childNodes[2].textContent.toLowerCase().includes(name.toLowerCase()) && $allCards[i].getAttribute('roles').toLowerCase().includes(currentTag.textContent.toLowerCase())) {
+    if ($allCards[i].childNodes[0].childNodes[3].textContent.toLowerCase().includes(name.toLowerCase()) && $allCards[i].getAttribute('roles').toLowerCase().includes(currentTag.textContent.toLowerCase())) {
       $allCards[i].style.display = '';
     } else {
       $allCards[i].style.display = 'none';
@@ -89,14 +99,87 @@ function toggleLore() {
   var $parentCard = event.target.closest('div');
   var $img = $parentCard.querySelector('img');
   var $lore = $parentCard.querySelector('p');
+  var $flipIcon = $parentCard.querySelector('i');
   if (!$parentCard.className.includes('lore')) {
     $parentCard.className += ' lore';
     $img.className = 'dark';
-    $lore.className = 'loreShown';
+    $lore.className = 'on';
+    $flipIcon.style.display = 'flex';
   } else {
     $parentCard.className = 'card';
     $img.className = '';
-    $lore.className = '';
+    $lore.className = 'off';
+    $flipIcon.style.display = 'none';
+  }
+}
+
+function flipCard() {
+  var $parentCard = event.target.closest('div');
+  var $lore = $parentCard.querySelector('p');
+  var $h3 = $parentCard.querySelector('h3');
+  var $h2 = $parentCard.querySelector('h2');
+  if ($parentCard.className === 'card lore') {
+    $h3.className = 'off';
+    $lore.className = 'off';
+    $h2.className = 'off';
+    $parentCard.style.transform = 'rotateY(180deg)';
+    $parentCard.className = 'card lore flipped';
+    var xhr2 = new XMLHttpRequest();
+    xhr2.open('GET', 'http://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion/' + $h2.textContent + '.json');
+    xhr2.responseType = 'json';
+    xhr2.addEventListener('load', function () {
+      var individualChampionData = xhr2.response;
+      var $iconSpan = document.createElement('span');
+      $iconSpan.className = 'ability-icons';
+      var $qIcon = document.createElement('img');
+      $qIcon.setAttribute('src', 'https://ddragon.leagueoflegends.com/cdn/11.22.1/img/spell/' + individualChampionData.data[$h2.textContent].spells[0].image.full);
+      var $wIcon = document.createElement('img');
+      $wIcon.setAttribute('src', 'https://ddragon.leagueoflegends.com/cdn/11.22.1/img/spell/' + individualChampionData.data[$h2.textContent].spells[1].image.full);
+      var $eIcon = document.createElement('img');
+      $eIcon.setAttribute('src', 'https://ddragon.leagueoflegends.com/cdn/11.22.1/img/spell/' + individualChampionData.data[$h2.textContent].spells[2].image.full);
+      var $rIcon = document.createElement('img');
+      $rIcon.setAttribute('src', 'https://ddragon.leagueoflegends.com/cdn/11.22.1/img/spell/' + individualChampionData.data[$h2.textContent].spells[3].image.full);
+      $iconSpan.prepend($qIcon, $wIcon, $eIcon, $rIcon);
+      $parentCard.appendChild($iconSpan);
+      var $name = document.createElement('p');
+      $name.className = 'yellow ability-name off';
+      var $description = document.createElement('p');
+      $description.className = 'ability-description off';
+      $iconSpan.append($name, $description);
+      var $ability = $parentCard.querySelectorAll('span.ability-icons > p');
+      $iconSpan.addEventListener('click', function () {
+        if (event.target === $qIcon) {
+          $ability[0].textContent = individualChampionData.data[$h2.textContent].spells[0].name;
+          $ability[0].className = 'yellow ability-name';
+          $ability[1].textContent = individualChampionData.data[$h2.textContent].spells[0].description;
+          $ability[1].className = 'ability-description';
+        } else if (event.target === $wIcon) {
+          $ability[0].textContent = individualChampionData.data[$h2.textContent].spells[1].name;
+          $ability[0].className = 'yellow ability-name';
+          $ability[1].textContent = individualChampionData.data[$h2.textContent].spells[1].description;
+          $ability[1].className = 'ability-description';
+        } else if (event.target === $eIcon) {
+          $ability[0].textContent = individualChampionData.data[$h2.textContent].spells[2].name;
+          $ability[0].className = 'yellow ability-name';
+          $ability[1].textContent = individualChampionData.data[$h2.textContent].spells[2].description;
+          $ability[1].className = 'ability-description';
+        } else if (event.target === $rIcon) {
+          $ability[0].textContent = individualChampionData.data[$h2.textContent].spells[3].name;
+          $ability[0].className = 'yellow ability-name';
+          $ability[1].textContent = individualChampionData.data[$h2.textContent].spells[3].description;
+          $ability[1].className = 'ability-description';
+        }
+      });
+    });
+    xhr2.send();
+  } else if ($parentCard.className === 'card lore flipped') {
+    $parentCard.style.transform = '';
+    $parentCard.className = 'card lore';
+    $h3.className = '';
+    $lore.className = 'on';
+    $h2.className = '';
+    var $abilityIcons = $parentCard.querySelector('.ability-icons');
+    $abilityIcons.remove();
   }
 }
 
